c5a8aafda0ec7de7d685d8fedde00976af5fd104
ndunhourowens <ndunhour@hotmail.com>
2016-07-16 20:51:13 -1000

    updated table to hide general list with searched list

--- client/SearchFields/searchFields.js
+++ client/SearchFields/searchFields.js
@@ -18,8 +18,12 @@ Template.searchFields.events({
     'click .js-searchBtn': function(event, template){
         var searchFor = $('.searchBox').val();
         Session.set('search', searchFor);
+        $('.found').css('display','inline');
+        $('.available').css('display','none');
+
     },
     'click .clickable': function(event, template){
         var row = $(this).closest('tr').children('td');
+        console.log(row.prevObject.prevObject[0]._id);
     }
 });
