2d385a6ca90f60c432eac8c4f1a2947bb8fef74e
ndunhourowens <ndunhour@hotmail.com>
2016-08-19 11:41:52 -1000

    create a function to get file from desktop and read it with js

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -1,5 +1,6 @@
 Template.admin.created = function(){
     this._barName = new ReactiveVar("Establishment Name");
+    this._img = new ReactiveVar();
 };
 
 Template.admin.rendered = function(){
@@ -11,6 +12,9 @@ Template.admin.helpers({
     },
     listBars: function(){
         return Template.instance()._barName.get();
+    },
+    img: function(){
+        return Template.instance()._img.get();
     }
 
 });
@@ -48,6 +52,36 @@ Template.admin.events({
         $('.title').val("");
         $('.artist').val("");
         $('.songId').val("");
+    },
+    'change #files': function(event, template){
+        var files = event.target.files; // FileList object
+
+            // Loop through the FileList and render image files as thumbnails.
+        for (var i = 0, f; f = files[i]; i++) {
+
+              // Only process image files.
+            if (!f.type.match('image.*')) {
+                continue;
+            }
+
+            var reader = new FileReader();
+
+              // Closure to capture the file information.
+            reader.onload = (function(theFile) {
+                return function(e) {
+                  // Render thumbnail.
+                  var span = document.createElement('span');
+                  span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                                    '" title="', escape(theFile.name), '"/>'].join('');
+                  document.getElementById('list').insertBefore(span, null);
+                };
+            })(f);
+
+              // Read in the image file as a data URL.
+              reader.readAsDataURL(f);
+        }
+
     }
+
 
 });
